74cb8c06dea299e6ca00f61a4c9a3166ceda260a
Joao00 <joao.neto.ninja@hotmail.com>
2017-04-23 15:34:01 -0300

    Configurando a base da api

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+require 'api_version_constraint' # libs/api_version_constraint.rb
+Rails.application.routes.draw do
+
+	# api.task-manager.dev:3000/resource
+	# defaults = especifica o formato de transferẽncia de dados
+	# constraints = cria um subdominio com o nome api.
+	# path = remove o namespace 'api' da URL
+	namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+		# ApiVersionConstraint = recebe o número da versão no header.
+		namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+			
+		end
+	end
+end
