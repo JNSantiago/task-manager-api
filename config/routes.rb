6a61fedf3c6805315ae15f52747348c6081f2366
Joao00 <joao.neto.ninja@hotmail.com>
2017-04-25 20:29:25 -0300

    Criando usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
 	# defaults = especifica o formato de transferẽncia de dados
 	# constraints = cria um subdominio com o nome api.
 	# path = remove o namespace 'api' da URL
-	namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+	namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
 		# ApiVersionConstraint = recebe o número da versão no header.
-		namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
-			resources :users, only: [:show]
+		namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
+			resources :users, only: [:show, :create]
 		end
 	end
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -32,7 +32,39 @@ RSpec.describe 'Users API', type: :request do
 				expect(response).to have_http_status(404)
 			end
 		end
+	end
 
+	describe "POST /users" do
+		before do
+			headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+			post '/users', params: { user: user_params }, headers: headers
+		end
+
+		context "Quando os parametros da requisição forem válidos" do
+			let(:user_params) { FactoryGirl.attributes_for(:user) }
+
+			it 'retorne 201' do
+				expect(response).to have_http_status(201)
+			end
+
+			it 'retorne json para o usuario' do
+				user_response = JSON.parse(response.body)
+				expect(user_response['email']).to eq(user_params[:email])
+			end
+		end
+
+		context "Quando os parametros da requisição forem invalidos" do
+			let(:user_params) {attributes_for(:user, email: 'invalid_email@')}
+
+			it 'retorne 422' do
+				expect(response).to have_http_status(422)
+			end
+
+			it 'retorne json para o usuario' do
+				user_response = JSON.parse(response.body)
+				expect(user_response).to have_key('errors')
+			end
+		end
 	end
 
 end
