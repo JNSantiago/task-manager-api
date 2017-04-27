d559b2fd48c9b44542b06befa9dd35fd6c8c6ea0
Joao00 <joao.neto.ninja@hotmail.com>
2017-04-26 23:52:08 -0300

    adicionando token usuario

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,25 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 	let(:user) { FactoryGirl.build(:user) }
 
-  it { is_expected.to validate_presence_of(:nome) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to validate_confirmation_of(:password) }
   it { is_expected.to allow_value("exemplo@exe.com").for(:email) }
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
 
+  describe '#generate_authentication_token!' do
+  	it 'gera um token unico' do
+  		allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+  		user.generate_authentication_token!
+
+  		expect(user.auth_token).to eq('abc123xyzTOKEN')
+  	end
+
+  	it 'gera outro token se já existe um no banco' do
+  		allow(Devise).to receive(:friendly_token).and_return('abc123xyzass', 'abc123xyzass', 'abc123xyzassdd')
+  		existing_user = create(:user)
+  		user.generate_authentication_token!
+
+  		expect(user.auth_token).not_to eq(existing_user)
+  	end
+  end
 end
